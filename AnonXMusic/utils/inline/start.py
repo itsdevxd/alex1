df615e2103b453f32121b76d998580a061b057f3
Dev meena <btwdevxd@gmail.com>
2024-06-23 22:51:43 +0530

    Update start.py
    Signed-off-by: Dev meena <btwdevxd@gmail.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/Dpz_store"),
+            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/do_lafz1"),
         ],
     ]
     return buttons
@@ -26,13 +26,11 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_9"], url="https://t.me/TheFriendsChatting"),
+            InlineKeyboardButton(text=_["S_B_9"], url="https://t.me/The_Royal_Chatting"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/Dpz_store"),
-        ],
-        [ InlineKeyboardButton(text=_["S_B_10"], url=f"https://telegram.dog/SignUp_WithdrawBot?start=Njk0NzIxNTUyMg==")
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/Do_lafz1"),
         ],
     ]
     return buttons
