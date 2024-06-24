d3e6cbd70732a3e8f68fbc88a5542cccbb03e8bc
Dev meena <btwdevxd@gmail.com>
2024-06-24 11:10:45 +0530

    Update config.py
    Signed-off-by: Dev meena <btwdevxd@gmail.com>

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BtwDevX/Btw_Dev",
+    "https://github.com/BtwDevX/Btwdev",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DPZ_STORE")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheFriendsChatting")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Do_lafz1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Royal_Chatting")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
